cb50f122702a9ae070f6f8245ccc032df36415dc
Bailey Brooks <bailey@github.com>
2023-03-30 11:24:18 +0000

    add lighting and model with subdivision surface applied

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
 import * as THREE from 'three';
 import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js'
 
 // init
 
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-camera.position.z = 1;
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-const myscene = new THREE.Scene();
-const loader = new GLTFLoader();
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 1, 20000 );
+camera.position.set( 1, 1, 20 );
+const renderer = new THREE.WebGLRenderer( { alpha: true, antialias: true } );
+renderer.setClearColor( 0xffffff, 0);
+renderer.toneMapping = THREE.ACESFilmicToneMapping;
+renderer.toneMappingExposure = 1;
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
 
-loader.load( './assets/thisisbowie.glb', function ( gltf ) {
+// controls
+const controls = new OrbitControls( camera, renderer.domElement ); 
 
-	myscene.add( gltf.scene );
+// lights
+const ambient = new THREE.PointLight(0xFFFFFF, 1);
+ambient.position.set(0, 100, 10);
+scene.add(ambient);
 
+const ambient2 = new THREE.PointLight(0xFFFFFF, 1);
+ambient2.position.set(10, 0, 100);
+scene.add(ambient2);
 
-}, undefined, function ( error ) {
+//load gltf file
+var loader = new GLTFLoader();				
+	loader.load( './assets/thisisbowie2.gltf', function ( gltf ) {
+	var object = gltf.scene;				
+	gltf.scene.scale.set( 2, 2, 2 );			   
+	gltf.scene.position.x = 0;				    //Position (x = right+ left-) 
+        gltf.scene.position.y = 0;				    //Position (y = up+, down-)
+	gltf.scene.position.z = 0;	
+    gltf.scene.rotation.x = 90 * Math.PI/180;		    //Position (z = front +, back-)
+	
+	scene.add( gltf.scene );
+    animate();
+    // renderer.render( scene, camera);
+	});	 
 
-	console.error( error );
+function animate() {
 
-} );
-
-renderer.render(myscene, camera);
-
-// const renderer = new THREE.WebGLRenderer( { antialias: true } );
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// renderer.setAnimationLoop( animation );
-// document.body.appendChild( renderer.domElement );
-
-// // animation
-
-// function animation( time ) {
-
-// 	gltf.rotation.x = time / 2000;
-// 	gltf.rotation.y = time / 1000;
-
-// 	renderer.render( scene, camera );
-
-// }
+    requestAnimationFrame( animate );
+    
+    // required if controls.enableDamping or controls.autoRotate are set to true
+    controls.update();
+    
+    renderer.render( scene, camera );
+    
+    }
