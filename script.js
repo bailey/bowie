24bc37420cd32ed95c0c5e8466d848423ea35fed
Bailey Brooks <bailey@github.com>
2023-03-30 15:43:27 +0000

    pls work

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import { GLTFLoader } from '/workspaces/bowie/node_modules/three/examples/jsm/loaders/GLTFLoader.js'
-import { OrbitControls } from '/workspaces/bowie/node_modules/three/examples/jsm/controls/OrbitControls.js'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 // init 
 
